3c28983febaf965251afb2329cbb9e5e461582b8
Gabriel Fauth <gabrielfauth@outlook.com>
2022-07-31 00:05:00 -0400

    removendo deploy heroku

--- helpdesk/settings.py
+++ helpdesk/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,5 +126,3 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-django_heroku.settings(locals())
